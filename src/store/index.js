ac110751bd1eb528883eb09b89254504971b7ac5
xujintao <1101916341@qq.com>
2020-05-27 16:43:32 +0800

    init-目录

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import reduxThunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 import reducer from './reducers'
 //安装redux-devtools-extension的可视化工具
 import { composeWithDevTools } from 'redux-devtools-extension'
@@ -7,6 +7,6 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 const composeEnhancers = process.env.NODE_ENV !== 'production' ? composeWithDevTools({}) : compose;
 const store = createStore(
     reducer,
-    composeEnhancers(applyMiddleware(reduxThunk)),
+    composeEnhancers(applyMiddleware(thunk)),
 )
 export default store
